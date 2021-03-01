b042f4e450ebc9f9e572e685f6ba5a9c5965eb57
Robin van Westrenen <robin.van.westrenen@rws.nl>
2021-03-01 22:10:33 +0100

    Single thread default

--- draw.py
+++ draw.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
-import sys, math, cairo, gi
+import sys, math, cairo, gi, gtk
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 
 try:
     gi.require_foreign("cairo")
 except ImportError:
     print("cairo missing :(")
 
-n = int(sys.argv[1]) # thread number of this worker
-m = int(sys.argv[2]) # total number of threads
+if (len(sys.argv) == 3):
+  n = int(sys.argv[1]) # thread number of this worker
+  m = int(sys.argv[2]) # total number of threads
+else:
+  single = True
 
-nr    = 8784 # Total number of time steps.
+nr    = 10   #8784 # Total number of time steps.
 ar    = 0.6  # Size of X compared to Y.
 x0    = 0    # Starting position: X
 y0    = 27   # Starting position: y
@@ -20,9 +26,21 @@ gs    = 57   # Grid size in pixels.
 vs    = 1/15 # Vector size in pixels per meter/second
 step  = 1    # Increase to draw less vectors.
 
+# Select file
+dialog = Gtk.FileChooserDialog()
+dialog.set_title("NetCDF file")
+dialog.set_action(Gtk.FileChooserAction.OPEN)
+dialog.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OK, Gtk.ResponseType.OK)
+
+# Sanity check for file selection
+if (dialog.run() != Gtk.ResponseType.OK):
+  print("No file selected... Bye!")
+  quit()
+
 # Get vector subset from file.
+print("Reading dataset: "+dialog.get_filename())
 from netCDF4 import Dataset
-ds = Dataset("download.nc", "r", format="NETCDF4")
+ds = Dataset(dialog.get_filename(), "r", format="NETCDF4")
 u = ds['u10'][0:nr,y0:y0+ytot,x0:x0+xtot]
 v = ds['v10'][0:nr,y0:y0+ytot,x0:x0+xtot]
 ds.close()
@@ -55,11 +73,11 @@ def drawVector(x,y,u,v,ms,ctx):
 # Black -> Areas -> Vectors.
 def draw(image, ctx):
     global t
-    if (t%m==n): # For multi-threading.
+    if (single or t%m==n): # For multi-threading.
       ctx.set_source_rgb(0, 0, 0)
       ctx.paint()
-      for y in xRange:
-        for x in yRange:
+      for y in yRange:
+        for x in xRange:
           ut = u[t,y,x]
           vt = v[t,y,x]
           ms = math.sqrt(ut*ut+vt*vt)
