78850667ff6ee2a64b89801c8e2f9a783a8cc44d
Robin van Westrenen <robin.van.westrenen@rws.nl>
2021-03-13 04:19:25 +0100

    Interpolation added and some work in progress.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# ncviz
 NetCDF visualization
 
-March 2nd 2021: https://youtu.be/CLipkVb85ug
+March 13th 2021: https://youtu.be/
 
-March 1st 2021: https://youtu.be/R5UTACnt9MY
+March  2nd 2021: https://youtu.be/CLipkVb85ug
+
+March  1st 2021: https://youtu.be/R5UTACnt9MY
+
+$ (for i in {0..365}; do echo "`printf "%0,6f\n" $(bc -q <<< scale=2;$i*24/30)` [enter] drawtext reinit 'fontfile=FreeSerif.ttf:text=`faketime -f '2020-1-1 00:00:00' date -d "+$i day" +'%-d %B'`';"; done) > text2
+$ ffmpeg -r 60 -i output/wind_%5d.png -r 60 -filter_complex "sendcmd=filename=./dates.txt,drawtext=text='':fontsize=64:fontcolor=white:x=1700:y=60" -i ~/Downloads/Exhale\ -\ Jeremy\ Blake.mp3 -b:v 68M -c:a copy -bf 2 test.mp4
--- draw.py
+++ draw.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 
-import sys, math, cairo, gi, shapefile
+import sys, math, numpy, cairo, gi, shapefile, threading, time
+from scipy import interpolate
 
 try:
     gi.require_foreign("cairo")
 except ImportError:
     print("cairo missing :(")
 
-nr    = 8784 # Total number of time steps.
-ar    = 0.6  # Size of X compared to Y.
-x0    = 0    # Starting position: X
-xn    = 113  # Number of X steps: max 113 in DCSMv6
-y0    = 27   # Starting position: y
-yn    = 65   # Number of Y steps: max 85 in DCSMv6
-gs    = 57   # Grid size in pixels.
-vs    = 1/15 # Vector size in pixels per meter/second
-step  = 1    # Increase to draw less vectors.
+# Performance and output
+th    = 6         # Number of threads.
+outx  = 1920      # Pixels X.
+outy  = 1080      # Pixels Y.
+gs    = 20        # Grid size in pixels.
+
+# Coordinates
+lat0 = -15.0     # Starting position: X
+latn =  13.0     # Number of X steps: max 113 in DCSMv6
+latd = lat0-latn
+lon0 =  59.5     # Starting position: y
+lonn =  48.5     # Number of Y steps: max 85 in DCSMv6
+lond = lon0-lonn
+ar   = (outx/outy)/(latd/lond) # Size of X compared to Y.
+
+vs    = 1/20 # Vector size in pixels per meter/second
+get   = 2
+step  = 4   # Increase to draw less vectors.
 
 if (len(sys.argv) == 4):
   n = int(sys.argv[2]) # thread number of this worker
@@ -47,10 +57,20 @@ else:
 print("Reading dataset: " + filename)
 from netCDF4 import Dataset
 ds = Dataset(filename, "r", format="NETCDF4")
-u = ds['u10'][0:nr,y0:yn,x0:xn]
-v = ds['v10'][0:nr,y0:yn,x0:xn]
-bbox = [-15,43,13,57.5]
+tn = ds.dimensions['time'].size
+u = [None for i in range(tn)]
+v = [None for i in range(tn)]
+tsx = numpy.arange(-15.0,13.0,0.14)
+tsy = numpy.arange(48.5,59.5,0.14*ar)
+for i in range(0,tn):
+  dsut = interpolate.interp2d(ds.variables['longitude'][:],ds.variables['latitude'][:],ds.variables['u10'][i], kind='cubic')
+  dsvt = interpolate.interp2d(ds.variables['longitude'][:],ds.variables['latitude'][:],ds.variables['v10'][i], kind='cubic')
+  u[i] = dsut(tsx,tsy)
+  v[i] = dsvt(tsx,tsy)
+  dsut = dsvt = None
 ds.close()
+bbox = [-15,43,13,57.5]
+
 
 # Get countries from shapefile.
 sf = shapefile.Reader("lib/countries.shp")
@@ -62,17 +82,17 @@ for sh in shs:
   countries.append(sh)
 
 # Plot countries.
-overlay = cairo.ImageSurface(cairo.FORMAT_ARGB32, int((xn-x0)*gs*ar), (yn-y0)*gs)
+overlay = cairo.ImageSurface(cairo.FORMAT_ARGB32, int((xn-x0)*gs), (yn-y0)*gs)
 ctx2 = cairo.Context(overlay)
 ctx2.set_line_width(3)
 ctx2.set_line_join(cairo.LINE_JOIN_BEVEL)
 for c in countries:
-  ctx2.move_to((c.points[0][0]-bbox[0])*ar*gs*10,-(c.points[0][1])*gs)
+  ctx2.move_to((c.points[0][0]-bbox[0])*ar*60*10,-(c.points[0][1])*60)
   for i, p in enumerate(c.points):
     if i in c.parts:
-      ctx2.move_to((p[0]-bbox[0])*gs*4*ar,-(p[1]-bbox[3])*gs*4)
+      ctx2.move_to((p[0]-bbox[0])*60*4*ar,-(p[1]-bbox[3])*60*4)
     else:
-      ctx2.line_to((p[0]-bbox[0])*gs*4*ar,-(p[1]-bbox[3])*gs*4)
+      ctx2.line_to((p[0]-bbox[0])*60*4*ar,-(p[1]-bbox[3])*60*4)
   #ctx2.set_source_rgba(1, 1, 1, 0.5)
   #ctx2.set_operator(cairo.Operator.SOURCE)
   #ctx2.fill_preserve()
@@ -80,55 +100,96 @@ for c in countries:
   ctx2.set_source_rgba(1, 1, 1, 0.5)
   ctx2.stroke()
 
-xRange = range(0,(xn-x0))
-yRange = range(0,(yn-y0))
-t = 0 # Time/frame counter.
-
-image = cairo.ImageSurface(cairo.FORMAT_ARGB32, int((xn-x0)*gs*ar), (yn-y0)*gs)
-ctx = cairo.Context(image)
+# Clean up memory.
+countries = sf = shs = None
+xRange = range(0,xn)
+yRange = range(0,yn)
 
 # Background of vector area.
 def drawArea(x,y,ms,ctx):
     ctx.set_source_rgb(3-(ms/7.5), 1-(ms/15), 1-(ms/15))
-    ctx.rectangle(x*gs,y*gs,gs,gs)
+    ctx.rectangle(x*gs,2160-y*gs,gs,-gs)
     ctx.fill()   
 
 # The vector itself.
 def drawVector(x,y,u,v,ms,ctx):
+    y = 107-y
     ctx.set_line_width(4)
     ctx.set_source_rgba(1, 1, 1, ms/15)
-    ctx.move_to(x*gs+(gs*ar)/2,y*gs+gs/2)
+    ctx.move_to(x*gs+(gs)/2,y*gs+gs/2)
     ctx.rel_line_to(u*gs*vs*step,-v*gs*vs*step)
     ctx.close_path()
     ctx.stroke()
     ctx.set_source_rgb(1, 1, 1)
-    ctx.arc(x*gs+(gs*ar)/2+(u)*gs*vs*step,y*gs+gs/2+(-v)*gs*vs*step,5*(ms/20),0,2*math.pi)
+    ctx.arc(x*gs+(gs)/2+(u)*gs*vs*step,y*gs+gs/2+(-v)*gs*vs*step,5*(ms/20),0,2*math.pi)
     ctx.fill()
 
 # Black -> Areas -> Vectors.
-def draw(image, ctx):
-    global t
+def draw(image, ctx, n):
     global overlay
-    if (single or t%m==n): # For multi-threading.
-      ctx.set_source_rgb(0, 0, 0)
-      ctx.paint()
-      for y in yRange:
-        for x in xRange:
-          ut = u[t,y,x]
-          vt = v[t,y,x]
+    ctx.set_source_rgb(0, 0, 0)
+    ctx.paint()
+    for y in xRange:
+      for x in yRange:
+        td[n]['zero'] = time.time()
+        ut = u[t[n]][x][y]
+        vt = v[t[n]][x][y]
+        td[n]['matrix'] += time.time() - td[n]['zero']
+        td[n]['zero'] = time.time()
+        ms = math.sqrt(ut*ut+vt*vt)
+        td[n]['math'] += time.time() - td[n]['zero']
+        td[n]['zero'] = time.time()
+        drawArea(y,x,ms,ctx)
+        td[n]['area'] += time.time() - td[n]['zero']
+    ctx.set_source_surface(overlay)
+    ctx.paint()
+    for y in xRange:
+      for x in yRange:
+        if (x%step == y%step==get):
+          td[n]['zero'] = time.time()
+          ut = u[t[n]][x][y]
+          vt = v[t[n]][x][y]
+          td[n]['matrix'] += time.time() - td[n]['zero']
+          td[n]['zero'] = time.time()
           ms = math.sqrt(ut*ut+vt*vt)
-          drawArea(x*ar,y,ms,ctx)
-      ctx.set_source_surface(overlay)
-      ctx.paint()
-      for y in yRange:
-        for x in xRange:
-          if (x%step == y%step==0):
-            ut = u[t,y,x]
-            vt = v[t,y,x]
-            ms = math.sqrt(ut*ut+vt*vt)
-            drawVector(x*ar,y,ut,vt,ms,ctx)
-      image.write_to_png("output/wind_"+str(format(t, '05'))+".png");
-    t += 1
+          td[n]['math'] += time.time() - td[n]['zero']
+          td[n]['zero'] = time.time()
+          drawVector(y,x,ut,vt,ms,ctx)
+          td[n]['vector'] += time.time() - td[n]['zero']
+    td[n]['zero'] = time.time()
+    image.write_to_png("output/wind_"+str(format(t[n], '05'))+".png");
+    td[n]['write'] = time.time() - td[n]['zero']
 
-while (t < nr):
-    draw(image,ctx)
+t=[0 for i in range(th)]
+td=[0 for i in range(th)]
+ctx=[0 for i in range(th)]
+image=[0 for i in range(th)]
+
+def worker(n,m):
+    while (t[n] < tn):
+      if (t[n]%m==n):
+        draw(image[n],ctx[n],n)
+        print("t:%d written by %d/%d" % (t[n],n+1,m))
+      t[n] += 1
+
+workers = list()
+tt = time.time()
+
+
+for index in range(th):
+    t[index] = 0 # Time-step counter at 0.
+    image[index] = cairo.ImageSurface(cairo.FORMAT_ARGB32, int((xn-x0)*gs), (yn-y0)*gs)
+    ctx[index] = cairo.Context(image[index])
+    ctx[index].set_antialias(cairo.Antialias.FAST)
+    td[index] = {'zero':0,'matrix':0,'math':0,'area':0,'vector':0,'write':0}
+    w = threading.Thread(target=worker, args=(index,th), daemon=True)
+    workers.append(w)
+    w.start()
+
+for w in workers:
+    w.join()
+
+for index, tdn in enumerate(td):
+    print("%d: matrix=%.2f, math=%.2f, area=%.2f, vector=%.2f, write=%.2f" % (index+1,tdn['matrix'],tdn['math'],tdn['area'],tdn['vector'],tdn['write']))
+
+print("total: %.2f seconds" % (time.time()-tt))
