4d98026427a1d7b2e17bc641b8c0fb8f7e0663af
Robin van Westrenen <robin.van.westrenen@rws.nl>
2021-03-14 23:46:31 +0100

    Temperature as input.

--- draw.py
+++ draw.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys, math, numpy, cairo, gi, shapefile, threading, time
+from datetime import datetime, timezone, timedelta
 from scipy import interpolate
 
 try:
@@ -10,8 +11,8 @@ except ImportError:
 
 # Performance and output
 th    = 6         # Number of threads.
-outx  = 1920      # Pixels X.
-outy  = 1080      # Pixels Y.
+outx  = 3840      # Pixels X.
+outy  = 2160      # Pixels Y.
 gs    = 20        # Grid size in pixels.
 
 # Coordinates
@@ -21,11 +22,13 @@ latd = lat0-latn
 lon0 =  59.5     # Starting position: y
 lonn =  48.5     # Number of Y steps: max 85 in DCSMv6
 lond = lon0-lonn
-ar   = (outx/outy)/(latd/lond) # Size of X compared to Y.
-
-vs    = 1/20 # Vector size in pixels per meter/second
+#ar   = (outx/outy)/(latd/lond) # Size of X compared to Y.
+ar = 0.6
+vs    = 1/15 # Vector size in pixels per meter/second
 get   = 2
 step  = 4   # Increase to draw less vectors.
+
+epoch = datetime(1900, 1, 1, 0, 0, tzinfo=timezone.utc)
 
 if (len(sys.argv) == 4):
   n = int(sys.argv[2]) # thread number of this worker
@@ -49,6 +52,7 @@ else:
   # Sanity check for file selection
   if (dialog.run() == Gtk.ResponseType.OK):
     filename = dialog.get_filename()
+    dialog.destroy()
   else:
     print("No file selected... Bye!")
     quit()
@@ -57,35 +61,38 @@ else:
 print("Reading dataset: " + filename)
 from netCDF4 import Dataset
 ds = Dataset(filename, "r", format="NETCDF4")
+print("Reading dataset finished; interpolating data.")
+t0 = 0
 tn = ds.dimensions['time'].size
+#t0 = 1000 # test
+#tn = 100 # test
 u = [None for i in range(tn)]
 v = [None for i in range(tn)]
+ts = [None for i in range(tn)]
+temp = [None for i in range(tn)]
 tsx = numpy.arange(-15.0,13.0,0.14)
 tsy = numpy.arange(48.5,59.5,0.14*ar)
 for i in range(0,tn):
-  dsut = interpolate.interp2d(ds.variables['longitude'][:],ds.variables['latitude'][:],ds.variables['u10'][i], kind='cubic')
-  dsvt = interpolate.interp2d(ds.variables['longitude'][:],ds.variables['latitude'][:],ds.variables['v10'][i], kind='cubic')
+  dsut = interpolate.interp2d(ds.variables['longitude'][:],ds.variables['latitude'][:],ds.variables['u10'][t0+i], kind='cubic')
+  dsvt = interpolate.interp2d(ds.variables['longitude'][:],ds.variables['latitude'][:],ds.variables['v10'][t0+i], kind='cubic')
+  dstt = interpolate.interp2d(ds.variables['longitude'][:],ds.variables['latitude'][:],ds.variables['t2m'][t0+i], kind='cubic')
   u[i] = dsut(tsx,tsy)
   v[i] = dsvt(tsx,tsy)
-  dsut = dsvt = None
+  ts[i] = ds.variables['time'][t0+i]
+  temp[i] = dstt(tsx,tsy)
 ds.close()
-bbox = [-15,43,13,57.5]
-
+print("Interpolating data finished; plotting countries.")
 
 # Get countries from shapefile.
 sf = shapefile.Reader("lib/countries.shp")
-shs = sf.shapes()
-countries = []
-
-for sh in shs:
-  #if (sh.bbox[0] > bbox[0] and sh.bbox[2] < bbox[2] and sh.bbox[1] > bbox[1] and sh.bbox[3] > bbox[3]):
-  countries.append(sh)
+countries = sf.shapes()
 
 # Plot countries.
-overlay = cairo.ImageSurface(cairo.FORMAT_ARGB32, int((xn-x0)*gs), (yn-y0)*gs)
+overlay = cairo.ImageSurface(cairo.FORMAT_ARGB32, outx, outy)
 ctx2 = cairo.Context(overlay)
 ctx2.set_line_width(3)
 ctx2.set_line_join(cairo.LINE_JOIN_BEVEL)
+bbox = [-15,43,13,57.5]
 for c in countries:
   ctx2.move_to((c.points[0][0]-bbox[0])*ar*60*10,-(c.points[0][1])*60)
   for i, p in enumerate(c.points):
@@ -93,21 +100,20 @@ for c in countries:
       ctx2.move_to((p[0]-bbox[0])*60*4*ar,-(p[1]-bbox[3])*60*4)
     else:
       ctx2.line_to((p[0]-bbox[0])*60*4*ar,-(p[1]-bbox[3])*60*4)
-  #ctx2.set_source_rgba(1, 1, 1, 0.5)
-  #ctx2.set_operator(cairo.Operator.SOURCE)
-  #ctx2.fill_preserve()
   ctx2.set_operator(cairo.Operator.SOURCE)
   ctx2.set_source_rgba(1, 1, 1, 0.5)
   ctx2.stroke()
 
+print("Plotting countries finished... start rendering frames.")
+
 # Clean up memory.
 countries = sf = shs = None
-xRange = range(0,xn)
-yRange = range(0,yn)
+xRange = range(0,int(outx/gs))
+yRange = range(0,int(outy/gs))
 
 # Background of vector area.
-def drawArea(x,y,ms,ctx):
-    ctx.set_source_rgb(3-(ms/7.5), 1-(ms/15), 1-(ms/15))
+def drawArea(x,y,tt,ms,ctx):
+    ctx.set_source_rgb(3-(ms/7.5), (1-(ms/15))+(tt-293)/30, (1-(ms/15))-(tt-293)/30)
     ctx.rectangle(x*gs,2160-y*gs,gs,-gs)
     ctx.fill()   
 
@@ -124,6 +130,16 @@ def drawVector(x,y,u,v,ms,ctx):
     ctx.arc(x*gs+(gs)/2+(u)*gs*vs*step,y*gs+gs/2+(-v)*gs*vs*step,5*(ms/20),0,2*math.pi)
     ctx.fill()
 
+# Draw info.
+def drawInfo(ctx,timestamp):
+    ctx.set_source_rgba(0,0,0,0.7)
+    ctx.rectangle(20,20,420,50)
+    ctx.fill()
+    ctx.set_source_rgba(1,1,1,0.8)
+    ctx.move_to(25, 55)
+    ctx.set_font_size(30)
+    ctx.show_text(timestamp.strftime("%Y: %B %d %H:00"))
+
 # Black -> Areas -> Vectors.
 def draw(image, ctx, n):
     global overlay
@@ -134,12 +150,13 @@ def draw(image, ctx, n):
         td[n]['zero'] = time.time()
         ut = u[t[n]][x][y]
         vt = v[t[n]][x][y]
+        tt = temp[t[n]][x][y]
         td[n]['matrix'] += time.time() - td[n]['zero']
         td[n]['zero'] = time.time()
         ms = math.sqrt(ut*ut+vt*vt)
         td[n]['math'] += time.time() - td[n]['zero']
         td[n]['zero'] = time.time()
-        drawArea(y,x,ms,ctx)
+        drawArea(y,x,tt,ms,ctx)
         td[n]['area'] += time.time() - td[n]['zero']
     ctx.set_source_surface(overlay)
     ctx.paint()
@@ -156,6 +173,7 @@ def draw(image, ctx, n):
           td[n]['zero'] = time.time()
           drawVector(y,x,ut,vt,ms,ctx)
           td[n]['vector'] += time.time() - td[n]['zero']
+    drawInfo(ctx,(epoch+timedelta(hours=int(ts[t[n]]))))
     td[n]['zero'] = time.time()
     image.write_to_png("output/wind_"+str(format(t[n], '05'))+".png");
     td[n]['write'] = time.time() - td[n]['zero']
@@ -178,7 +196,7 @@ tt = time.time()
 
 for index in range(th):
     t[index] = 0 # Time-step counter at 0.
-    image[index] = cairo.ImageSurface(cairo.FORMAT_ARGB32, int((xn-x0)*gs), (yn-y0)*gs)
+    image[index] = cairo.ImageSurface(cairo.FORMAT_ARGB32, outx, outy)
     ctx[index] = cairo.Context(image[index])
     ctx[index].set_antialias(cairo.Antialias.FAST)
     td[index] = {'zero':0,'matrix':0,'math':0,'area':0,'vector':0,'write':0}
